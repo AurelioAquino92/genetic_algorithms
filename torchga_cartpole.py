a6383aa00302ddd24ef2ab44b4d2896f37cae5d7
Aurelio Aquino <aurelio.obmep@gmail.com>
2022-11-13 14:45:42 -0400

    saving and loading model instead of torchga object

--- torchga_cartpole.py
+++ torchga_cartpole.py
@@ -3,37 +3,39 @@ import pygad
 from torch import nn, tensor
 from pygad.torchga import predict, TorchGA
 import gym
+from time import time
 
 def fitness_func(solution, sol_idx):
     global torch_ga, model
 
     solution_fitness = 0
-    for _ in range(2000):
-        obs = env.reset()
+    for _ in range(20):
+        obs, _ = env.reset()
         inputs = numpy.append(obs, [obs, obs])
         done = False
         while not done:
-            data_input = tensor([inputs])
+            data_input = tensor(numpy.array([inputs]))
             prediction = predict(model=model,
                                 solution=solution,
                                 data=data_input)
             action = numpy.argmax(prediction.detach().numpy())
-            obs, reward, done, _ = env.step(action)
-            inputs = numpy.append(obs, inputs[4:])
+            obs, reward, done, _, _ = env.step(action)
+            inputs = numpy.append(obs, inputs[:-4])
             # env.render()
             solution_fitness += reward - numpy.abs(obs[0]) / 100 - numpy.abs(obs[2])
         
     return solution_fitness
 
 def callback_generation(ga_instance):
-    global last_fitness
+    global last_fitness, tempo
 
-    fitness = ga_instance.best_solution()[1].copy()
+    solution, fitness, _ = ga_instance.best_solution()
     if fitness > last_fitness:
         print("Generation = {generation}".format(generation=ga_instance.generations_completed))
         print("Fitness    = {fitness}".format(fitness=fitness))
         print("Change     = {change}".format(change=fitness - last_fitness))
         ga_instance.save('torchgacartpole')
+        numpy.save('torchmodel.npy', solution)
         # obs = env.reset()
         # done = False
         # while not done:
@@ -43,17 +45,16 @@ def callback_generation(ga_instance):
         #     env.render()
     else:
         print("No improvement. Generation = {generation} Fitness = {fitness}".format(generation=ga_instance.generations_completed, fitness=fitness))
-
+    print('Tempo: ', time()-tempo)
+    tempo = time()
     last_fitness = fitness
 
 env = gym.make('CartPole-v1')
 last_fitness = 0
 
-model = nn.Sequential(  nn.Linear(12, 20),
+model = nn.Sequential(  nn.Linear(12, 13),
                         nn.ReLU(),
-                        nn.Linear(20, 9),
-                        nn.ReLU(),
-                        nn.Linear(9, 2))
+                        nn.Linear(13, 2))
 torch_ga = TorchGA(model=model, num_solutions=50)
 
 if __name__ == "__main__":
@@ -69,10 +70,11 @@ if __name__ == "__main__":
                             mutation_type="random",
                             keep_parents=2,
                             on_generation=callback_generation,
-                            parallel_processing=['process', 1])
+                            parallel_processing=['process', 8])
 
-    # ga_instance = pygad.load('C:\\Users\\mnsaaqui\\Desktop\\python\\IA\\genetic_algorithms\\torchgacartpole')
+    ga_instance = pygad.load('torchgacartpole')
     # ga_instance.num_generations = 10
+    tempo = time()
     ga_instance.run()
     ga_instance.plot_fitness()
 
--- torchga_cartpole_run.py
+++ torchga_cartpole_run.py
@@ -1,30 +1,24 @@
-from numpy import argmax, append, array
-from pygad import load
+from numpy import argmax, append, array, load
 from gym import make
 from torch import nn, tensor
 from pygad.torchga import predict
-from torchga_cartpole import fitness_func, callback_generation
 
-torchga_instance_run = load('C:\\Users\\mnsaaqui\\Desktop\\python\\IA\\genetic_algorithms\\torchgacartpole')
-torchga_instance_run.parallel_processing = None
-env_run = make('CartPole-v1')
-model = nn.Sequential(  nn.Linear(12, 20),
+env_run = make('CartPole-v1', render_mode='human')
+model = nn.Sequential(  nn.Linear(12, 13),
                         nn.ReLU(),
-                        nn.Linear(20, 9),
-                        nn.ReLU(),
-                        nn.Linear(9, 2))
-solution = torchga_instance_run.best_solution()[0]
+                        nn.Linear(13, 2))
+solution = load('torchmodel.npy')
 
 for episode in range(5):
-    obs = env_run.reset()
+    obs, _ = env_run.reset()
     inputs = append(obs, [obs, obs])
     done = False
     total_reward = 0
     while not done:
         prediction = predict(model, solution, tensor(array(inputs)))
         action = argmax(prediction.detach().numpy()) 
-        obs, rewards, done, info = env_run.step(action)
-        inputs = append(obs, inputs[4:])
+        obs, rewards, done, info, _ = env_run.step(action)
+        inputs = append(obs, inputs[:-4])
         env_run.render()
         total_reward += rewards
     print("Run: {run}   Reward: {rew}".format(run=episode+1, rew=total_reward))
